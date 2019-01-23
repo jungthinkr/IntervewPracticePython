aa176faea314654ef45569610d95f0fc447abd65
jungthinkr <aaron.jeffrey.liu@gmail.com>
2019-01-23 15:45:28 -0800

    done with chapter 3

--- 3.5_sort_stack.py
+++ 3.5_sort_stack.py
@@ -5,15 +5,44 @@ data structure (such as an array). The stack supports the following operations: 
 and isEmpty.
 """
 
-from Stack import Stack
+class Stack:
+  def __init__(self):
+    self.arr = []
+
+  def push(self, x):
+    self.arr.append(x)
+
+  def pop(self):
+    return self.arr.pop()
+
+  def peek(self):
+    if len(self.arr) > 0:
+      return self.arr[-1]
+    return None
+
+  def isEmpty(self):
+    return len(self.arr) == 0
+
+  def sort(self):
+    temp = Stack()
+    while not self.isEmpty():
+      element = self.pop()
+      while not temp.isEmpty() and temp.peek() < element:
+        self.push(temp.pop())
+      temp.push(element)
+    while not temp.isEmpty():
+      self.push(temp.pop())
+    
+  def printArr(self):
+    print(self.arr)
+
 
 if __name__ == '__main__':
   a = Stack()
-  b = Stack()
   a.push(1)
   a.push(2)
   a.push(3)
-
-
-
+  a.push(2)
+  a.sort()
+  a.printArr()
 
--- 3.6_animal_shelter.py
+++ 3.6_animal_shelter.py
@@ -2,7 +2,63 @@
 An animal shelter, which holds only dogs and cats, operates on a strictly
 "first in, first out" basis. People must adopt either the "oldest" (based on arrival time)
 of all animals in the shelter, or they can select whether they would prefer a dog or a cat
-(and will receive t he oldest animal of that type). They cannot select which specific animal they would like.
+(and will receive the oldest animal of that type). They cannot select which specific animal they would like.
 Create the data structure to maintain this system and implement operations such as enqueue, dequeueAny,
 dequeueDog, and dequeueCat. You may use the built-in LinkedList Data Structure
 """
+class Animal:
+  def __init__(self, name, kind):
+    self.name = name
+    self.kind = kind
+class Dog(Animal):
+  def __init__(self, name):
+    Animal.__init__(self, name, 'dog')
+class Cat(Animal):
+  def __init__(self, name):
+    Animal.__init__(self, name, 'cat')
+
+class AnimalShelter:
+  def __init__(self):
+    self.arr = []
+
+  def enqueue(self,animal):
+    self.arr.append(animal)
+
+  def dequeueAny(self):
+    if len(self.arr) > 0:
+      return self.arr.pop(0)
+    return None
+
+  def dequeueCat(self):
+    for i in range(len(self.arr)):
+      if self.arr[i].kind == 'cat':
+        return self.arr.pop(i)
+    return None
+
+  def dequeueDog(self):
+    for i in range(len(self.arr)):
+      if self.arr[i].kind == 'dog':
+        return self.arr.pop(i)
+    return None
+
+if __name__ == '__main__':
+  animalShelter = AnimalShelter()
+  animalShelter.enqueue(Dog('buddy'))
+  animalShelter.enqueue(Dog('obed'))
+  animalShelter.enqueue(Cat('mark'))
+  animalShelter.enqueue(Dog('hansel'))
+
+  print(animalShelter.dequeueCat().name)
+  print(animalShelter.dequeueDog().name)
+  print(animalShelter.dequeueAny().name)
+
+      
+  
+
+
+
+
+
+
+
+
