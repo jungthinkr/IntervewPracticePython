15bdadb407991eb40e249408861faf636df2680c
jungthinkr <jungthinkr@gmail.com>
2019-01-23 07:40:46 -0800

    add the rest of chapter 3

--- 3.4_queue_via_stacks.py
+++ 3.4_queue_via_stacks.py
@@ -2,17 +2,31 @@
 Implement a MyQueue class which implements a queue using two stacks.
 """
 
-from Stack import Stack
-
 class MyQueue:
   def __init__(self):
-    self.stackA = Stack()
-    self.stackB = Stack()
+    self.stackA = []
+    self.stackB = []
 
   def enqueue(self, x):
-    self.stackA.push(x)
+    self.stackA.append(x)
 
   def dequeue(self):
-    
+    if len(self.stackB) > 0:
+      return self.stackB.pop()
+
+    while self.stackA:
+      a=self.stackA.pop()
+      self.stackB.append(a)
+    return self.stackB.pop()
+
+
+if __name__ == '__main__':
+  q = MyQueue()
+  q.enqueue(1)
+  q.enqueue(2)
+  q.enqueue(3)
+  print(q.dequeue())
+  print(q.dequeue())
+  print(q.dequeue())
     
   
---
+++ 3.5_sort_stack.py
@@ -0,0 +1,19 @@
+"""
+Write a program to sort a stack such that the smallest items are on the top. 
+You can use an additional temporary stack, but you may not copy the elements into any other 
+data structure (such as an array). The stack supports the following operations: push, pop, peek,
+and isEmpty.
+"""
+
+from Stack import Stack
+
+if __name__ == '__main__':
+  a = Stack()
+  b = Stack()
+  a.push(1)
+  a.push(2)
+  a.push(3)
+
+
+
+
