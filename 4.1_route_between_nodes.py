5e28f8356d6964e60c9d0b84be4d48ad52a50584
jungthinkr <jungthinkr@gmail.com>
2019-01-24 07:48:32 -0800

    working on some chapter 4

--- 4.1_route_between_nodes.py
+++ 4.1_route_between_nodes.py
@@ -1,4 +1,39 @@
 """
-Given a directed graph, design an algorithm to find out whether there is a route betwee
+Given a directed graph, design an algorithm to find out whether there is a route between
 two nodes.
 """
+
+class Graph:
+  def __init__(self, val):
+    self.val = val
+    self.edges = []
+
+def hasRoute(a, b):
+  s = a.edges
+  while s:
+    nxt = s.pop()
+    if nxt == b:
+      return True
+    s.extend(nxt.edges)
+  return False
+    
+if __name__ == '__main__':
+  a = Graph(1)
+  b = Graph(2)
+  c = Graph(3)
+  d = Graph(4)
+  e = Graph(5)
+  f = Graph(6)
+
+  a.edges = [b,c,d]
+  b.edges = [d]
+  c.edges = [f]
+  d.edges = [e]
+  e.edges = [f]
+  f.edges = []
+
+  #find a to f
+  print(hasRoute(a, f))
+
+  
+
--- 4.2_minimal_tree.py
+++ 4.2_minimal_tree.py
@@ -2,4 +2,38 @@
 Given a sorted (increasing order) array with unique integer elements, write an algorithm to create
 a binary search tree with minimal height
 """
+# binary search tree left is lower and right is higher
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+def construct(arr):
+  arr.sort()
+  head = _construct(arr, 0, len(arr)-1)
+  #printHead(head)
+
+def printHead(head):
+  if not head:
+    return
+  print(head.val)
+  printHead(head.left)
+  printHead(head.right)
+
+def _construct(arr, l, r):
+  if l >= r:
+    print(l)
+    return None
+
+  mid = (l+r)/2
+  h = TreeNode(arr[mid])
+  h.left = _construct(arr, l, mid-1)
+  h.right = _construct(arr, mid+1, r)
+  return h
+
+if __name__ == '__main__':
+  arr = [1,2,3,4,5,6,7,3,2,5]
+  construct(arr)
 
